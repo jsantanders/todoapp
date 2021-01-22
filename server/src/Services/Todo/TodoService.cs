db8c58a2c24e3865d00d188cb66d8f1c353f763e
Jesus <jsantanders@outlook.com>
2021-01-22 11:16:02 -0400

    all: configure base files and directory

--- server/src/Services/Todo/TodoService.cs
+++ server/src/Services/Todo/TodoService.cs
@@ -1,14 +1,94 @@
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using TodoApp.Data;
+using models = TodoApp.Data.Models;
 
 namespace TodoApp.Services.Todo
 {
-    public class TodoService : Todos.TodosBase 
+    public class TodoService : Todos.TodosBase
     {
         private readonly TodoAppDbContext context;
-        
+
         public TodoService(TodoAppDbContext context)
         {
             this.context = context;
+        }
+
+        public override async Task<ListReply> List(Empty request, ServerCallContext context)
+        {
+            var reply = new ListReply();
+            var todos = await this.context.Todos.ToListAsync();
+            foreach (var todo in todos)
+            {
+                reply.Todos.Add(Serialize(todo));
+            }
+
+            return reply;
+        }
+
+        public override async Task<Todo> Create(Todo request, ServerCallContext context)
+        {
+            if (request.Text is null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, ""), "field text cannot be empty");
+            }
+
+            var todo = new models.Todo
+            {
+                Text = request.Text,
+                Done = request.Done
+            };
+
+            this.context.Todos.Add(todo);
+            await this.context.SaveChangesAsync();
+
+            return Serialize(todo);
+        }
+
+        public override async Task<Todo> Update(Todo request, ServerCallContext context)
+        {
+            var todo = await this.context.Todos.FindAsync(request.Name);
+            if (todo is null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, ""), $"todo with id {request.Name} does not exist");
+            }
+            if (request.Text is null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, ""), "field text cannot be empty");
+            }
+
+            todo.Text = request.Text;
+            todo.Done = request.Done;
+
+            await this.context.SaveChangesAsync();
+
+            return Serialize(todo);
+        }
+
+        public override async Task<Empty> Delete(DeleteRequest request, ServerCallContext context)
+        {
+            var todo = await this.context.Todos.FindAsync(request.Name);
+            if (todo is null)
+            {
+                throw new RpcException(new Status(StatusCode.NotFound, ""), $"todo with id {request.Name} does not exist");
+            }
+
+            this.context.Todos.Remove(todo);
+            await this.context.SaveChangesAsync();
+
+            return new Empty();
+        }
+
+        private Todo Serialize(models.Todo todo)
+        {
+            return new Todo
+            {
+                Name = todo.Id,
+                Text = todo.Text,
+                Done = todo.Done
+            };
         }
     }
 }
--- server/src/Startup.cs
+++ server/src/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using TodoApp.Data;
 using TodoApp.Services.Todo;
 
 namespace server
@@ -28,21 +30,31 @@ namespace server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
             services.AddGrpcHttpApi();
+            services.AddDbContext<TodoAppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("database")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TodoApp API", Version = "v1" });
             });
+            services.AddGrpcSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApp v1"));
 
             app.UseRouting();
+            app.UseCors();
 
             app.UseEndpoints(endpoints =>
             {
